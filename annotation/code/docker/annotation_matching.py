58a1b3b44953060dd64ee88e7ef36dd5d8dae7f8
chenbinkg <48574103+chenbinkg@users.noreply.github.com>
2025-10-31 14:27:45 +0800

    Import MongoDBOps in annotation_matching.py

--- annotation/code/docker/annotation_matching.py
+++ annotation/code/docker/annotation_matching.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import argparse
+from .mongodb import MongoDBOps
 
 # Add parent directory to path for imports when running from /opt/ml/processing/input/code/
 sys.path.insert(0, '/opt/ml/processing')
